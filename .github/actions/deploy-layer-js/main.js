f927c0b592ec1b19f7218f9ef094bdd63ea931a6
rodreinheimer <rodrigo.reinheimer@skechers.com>
2023-12-01 15:31:26 -0800

    test

--- .github/actions/deploy-layer-js/main.js
+++ .github/actions/deploy-layer-js/main.js
@@ -16,14 +16,8 @@ function run() {
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
     // 2) publish layer
-    exec.exec(`aws lambda publish-layer-version \
-    --layer-name ${layerName} \
-    --description ${layerDescription} \
-    --content S3Bucket=${bucket},S3Key=${packageName} \
-    --compatible-runtimes nodejs16.x`);
+    exec.exec(`aws lambda publish-layer-version --layer-name ${layerName} --description ${layerDescription} --content S3Bucket=${bucket},S3Key=${packageName} --compatible-runtimes nodejs16.x`);
 
-
-    
     //const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
 
     //core.setOutput('website-url', websiteUrl);
