157c4f6cdbb196751626fed8a8d46b8c9cfe5cc4
rodreinheimer <rodrigo.reinheimer@skechers.com>
2023-12-01 16:31:36 -0800

    test

--- .github/actions/aws-lambda-layer-load/main.ts
+++ .github/actions/aws-lambda-layer-load/main.ts
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 
 function run() {
     // 1) Get Input Values
