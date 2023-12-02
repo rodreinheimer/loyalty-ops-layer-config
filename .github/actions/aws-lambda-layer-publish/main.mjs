0b4cd89f01f369480e6362c7f9bcb8d04ddb5516
rodreinheimer <rodrigo.reinheimer@skechers.com>
2023-12-01 18:44:03 -0800

    test

--- .github/actions/aws-lambda-layer-publish/main.mjs
+++ .github/actions/aws-lambda-layer-publish/main.mjs
@@ -6,6 +6,7 @@ async function run() {
         const bucketName = core.getInput('bucket-name', { required: true });
         const zipFileName = core.getInput('zip-file-name', { required: true })
         const layerName = core.getInput('layer-name', { required: true });
+        const layerDescription = core.getInput('layer-description', { required: true });
         const lambdaConfig = {
             accessKeyId: process.env.AWS_ACCESS_KEY_ID,
             apiVersion: '2015-03-31',
@@ -15,14 +16,14 @@ async function run() {
             sslEnabled: true,
         }
         const client = new LambdaClient(lambdaConfig);
-        const input = { // PublishLayerVersionRequest
-            LayerName: layerName, // required
-            Description: "STRING_VALUE",
-            Content: { // LayerVersionContentInput
+        const input = { 
+            LayerName: layerName, 
+            Description: layerDescription,
+            Content: { 
                 S3Bucket: bucketName,
                 S3Key: zipFileName
             },
-            CompatibleRuntimes: [ // CompatibleRuntimes
+            CompatibleRuntimes: [ 
                 "nodejs16.x",
             ]
         };
